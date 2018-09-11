dc79a63b6f350d0c1215a326b1fd917c885754e8
zhangsan <zhangsan@itcast.cn>
2018-09-11 20:58:08 +0800

    9.11.5

--- pinyougou-parent/pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
+++ pinyougou-parent/pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -1,7 +1,28 @@
 package com.pinyougou.manager.controller;
 
+import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.sellergoods.service.BrandService;
+import jdk.nashorn.internal.ir.annotations.Reference;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+/**
+ * 这个注解相当于Controller加上ResponseBody注解
+ */
 @RestController
+@RequestMapping("/brand")
 public class BrandController {
+    /**
+     *
+     */
+    @Reference
+    private BrandService brandService;
+
+    @RequestMapping("/findAll")
+    public List<TbBrand> findAll() {
+        return brandService.findAll();
+    }
+
 }
