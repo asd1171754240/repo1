d0bcbbf88d4cd8028a7612fe1fcdfb200be90ec9
zhangsan <zhangsan@itcast.cn>
2018-09-11 14:43:53 +0800

    9.11.3

--- pinyougou-parent/pinyougou-pojo/src/main/java/com/pinyougou/pojo/TbGoodsExample.java
+++ pinyougou-parent/pinyougou-pojo/src/main/java/com/pinyougou/pojo/TbGoodsExample.java
@@ -1,10 +1,11 @@
 package com.pinyougou.pojo;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TbGoodsExample {
+public class TbGoodsExample{
     protected String orderByClause;
 
     protected boolean distinct;
