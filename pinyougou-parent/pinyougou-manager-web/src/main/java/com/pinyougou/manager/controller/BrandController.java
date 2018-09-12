9a21ba534040b40cd7168b30d2b7a47ca0331f60
zhangsan <zhangsan@itcast.cn>
2018-09-12 16:04:05 +0800

    9.12.1

--- pinyougou-parent/pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
+++ pinyougou-parent/pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -2,6 +2,7 @@ package com.pinyougou.manager.controller;
 
 import java.util.List;
 
+import entity.PageResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +14,17 @@ import com.pinyougou.sellergoods.service.BrandService;
 @RequestMapping("/brand")
 public class BrandController {
 
-	@Reference
-	private BrandService brandService;
-	
-	@RequestMapping("/findAll")
-	public List<TbBrand> findAll(){
-		return brandService.findAll();		
-	}
-	
+    @Reference
+    private BrandService brandService;
+
+    @RequestMapping("/findAll")
+    public List<TbBrand> findAll() {
+        return brandService.findAll();
+    }
+
+    @RequestMapping("/findPage")
+    public PageResult findPage(int page, int size) {
+        return brandService.findPage(page, size);
+    }
+
 }
--- pinyougou-parent/pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
+++ pinyougou-parent/pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -3,6 +3,7 @@ package com.pinyougou.sellergoods.service;
 import java.util.List;
 
 import com.pinyougou.pojo.TbBrand;
+import entity.PageResult;
 
 /**
  * 品牌接口
@@ -12,5 +13,7 @@ import com.pinyougou.pojo.TbBrand;
 public interface BrandService {
 
 	public List<TbBrand> findAll();
+
+	public PageResult findPage(int pageNum,int pageSize);
 	
 }
