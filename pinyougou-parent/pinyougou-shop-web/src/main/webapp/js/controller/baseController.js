7c176ab10d99a36393272074f2d7d43c11e811a0
zhangsan <zhangsan@itcast.cn>
2018-09-16 19:44:18 +0800

    9.16.3

---
+++ pinyougou-parent/pinyougou-shop-web/src/main/webapp/js/controller/baseController.js
@@ -0,0 +1,67 @@
+app.controller('baseController', function ($scope) {
+    /**
+     * 重新加载数据列表数据
+     */
+
+    $scope.reloadList = function () {
+        $scope.search($scope.paginationConf.currentPage, $scope.paginationConf.itemsPerPage)
+    }
+
+    // 分页控件配置currentPage:当前页
+    // totalItems:总记录数
+    // itemsPerPage:每页记录数
+    // perPageOptions:分页选项
+    // onChange :当页码变更后自动触发的方法
+    $scope.paginationConf = {
+        currentPage: 1,
+        totalItems: 10,
+        itemsPerPage: 10,
+        perPageOptions: [10, 20, 30, 40, 50],
+        onChange: function () {
+            $scope.reloadList();
+        }
+    };
+
+    $scope.selectIds = [];
+
+    $scope.updateSelection = function ($event, id) {
+        if ($event.target.checked) {
+
+            $scope.selectIds.push(id);//push向集合添加元素
+
+        } else {
+            var index = $scope.selectIds.indexOf(id)
+            $scope.selectIds.splice(index, 1);
+        }
+    }
+
+    $scope.jsonToString = function (jsonString, key) {
+
+        var json = JSON.parse(jsonString);
+        var value = "";
+
+        for (var i = 0; i < json.length; i++) {
+            if (i > 0) {
+                value += ",";
+            }
+            value += json[i][key];
+        }
+
+        return value;
+    }
+
+        $scope.jsonToString = function (jsonString, key) {
+            var json = JSON.parse(jsonString);//josn字符串怎么提取，没办法提取，所以转换为josn字符，然后做下一步操作
+            var value = "";/*这个value存储解析后，返回的东西 */
+            for (var i = 0; i < json.length; i++) {
+                if (i > 0) {
+                    value += ","
+                }
+                value += json[i][key];
+            }
+            return value;
+
+        }
+
+    }
+);
